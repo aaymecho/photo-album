e78cbd04735a2737d9accd255b494f5c7617b5bc
Slim <aaymecho3@gatech.edu>
2024-02-11 17:52:15 -0500

    updated env.py

--- utils/env.py
+++ utils/env.py
@@ -1,19 +1,19 @@
 # IAM User access configuration
-AWS_ACCESS_KEY="AKIATCKATVG576GTFFP_your_key"
-AWS_SECRET_ACCESS_KEY="RhVuXYVg9emAxwvO2pdt0oO9UAGR/kPbtYwE_your_key"
+AWS_ACCESS_KEY="YMy9GnLk0i9t+9d4mGILo2YHEGcJmEc2TaVGna+Z"
+AWS_SECRET_ACCESS_KEY="YMy9GnLk0i9t+9d4mGILo2YHEGcJmEc2TaVGna+Z"
 AWS_REGION="us-east-1"
 
 # S3 Bucket for photo objects
-PHOTOGALLERY_S3_BUCKET_NAME="photobucket-yourLastName-2024-4150"
+PHOTOGALLERY_S3_BUCKET_NAME="photobucket-aymecho-2024-4150"
 
 # MySQL Configuration
-RDS_DB_HOSTNAME= 'photogallerydb.cbkym040u.us-east-1.rds.amazonaws.com-your-endpoint'
+RDS_DB_HOSTNAME= 'photogallerydb.crmckames979.us-east-1.rds.amazonaws.com'
 RDS_DB_USERNAME='root'
-RDS_DB_PASSWORD='ECE4150128'
+RDS_DB_PASSWORD='Super1196'
 RDS_DB_NAME='photogallerydb'
 
 # DynamoDB Table
-DYNAMODB_TABLE='PhotoGallery or photogallerydb' #whichever you have mention during the setup
+DYNAMODB_TABLE='PhotoGallery' #whichever you have mention during the setup
 
 ###### INSERT NEW ENVIRONMENT VARIEABLES HERE ######
 
