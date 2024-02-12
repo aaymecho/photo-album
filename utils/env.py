f8fc382ee432d156078cce0038aa115d6101c1c1
slim <aaymecho3@gatech.edu>
2024-02-11 20:28:40 -0500

    updated env

--- utils/env.py
+++ utils/env.py
@@ -1,5 +1,5 @@
 # IAM User access configuration
-AWS_ACCESS_KEY="YMy9GnLk0i9t+9d4mGILo2YHEGcJmEc2TaVGna+Z"
+AWS_ACCESS_KEY="AKIA3QEG6KV2MHHT356H"
 AWS_SECRET_ACCESS_KEY="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"
 AWS_REGION="us-east-1"
 
@@ -13,7 +13,7 @@ RDS_DB_PASSWORD='Super1196'
 RDS_DB_NAME='photogallerydb'
 
 # DynamoDB Table
-DYNAMODB_TABLE='PhotoGallery' #whichever you have mention during the setup
+DYNAMODB_TABLE='photogallerydb' #whichever you have mention during the setup
 
 ###### INSERT NEW ENVIRONMENT VARIEABLES HERE ######
 
